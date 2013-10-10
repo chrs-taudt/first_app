da166c918d6255479896bdd288733935a8197ca0
chrs <christaudt@gmx.net>
2013-10-10 22:31:54 +0200

    erster commit fuer rubyOnrails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '30475fc1570c87e4d802a5b9fb5577b97547e979b75bd9a037fa35c6c678b43369e67d5595a7253552c51ea281cd332b72968e0cf91924cb87e105d58ed8f40e'
